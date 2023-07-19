ee58c11259e6a6a63dc046f41e07701a125299fa
Giuseppe Cifarelli <cifarelli1995@gmail.com>
2023-07-19 22:20:14 +0200

    Continuazione del Recap delle classi

--- class.ts
+++ class.ts
@@ -1,24 +1,28 @@
 // Si definiscono ISTANZE quegli oggetti generati direttamente dalle classi
 
+import { log } from "console"
+
 // TS fa largo uso delle classi, pertanto aggiunge novita' a JS quali i modificatori 
 // E' possibile aggiungere i modificatori durante la dichiarazione di variabili, parametri, funzioni, ecc.
 // public --> e' il modificatore di default degli elementi delle classi. Gli elementi public sono disponibili in classi, sottoclassi ed istanze
 // private --> l'elemento a cui e' aggiunto e' disponibile solo all' interno della classe attuale
 // protected --> e' disponibile sia nella classe attuale sia nella sottoclasse che la estende
-// static --> l' elemento static e' accessibile SENZA le istanze
+// static --> l' elemento static e' accessibile SENZA le istanze e viene ereditato dalle sottoclassi
 // readonly --> l' elemento e' di sola lettura, una volta definito non si potra' cambiare il valore
 
 class Persona {
     nome?:string // Il ? durante la tipizzazione aggiunge undefined al tipo della variabile, bloccando gli errori dovuti alla mancata inizializzazione
-    cognome!:string // Il ! ,invece, avvisa TS che l' inizializzazione sara' effettuata in seguito, bloccando gli errori
+    private cognome!:string // Il ! ,invece, avvisa TS che l' inizializzazione sara' effettuata in seguito, bloccando gli errori
     eta!:number // Sono due modi per bloccare gli errori di TS durante la tipizzazione degli elementi delle classi
     readonly id!:number
+    protected residenza:string
 
-    constructor(nome:string, cognome:string, eta:number, id:number) {
+    constructor(nome:string, cognome:string, eta:number, id:number, residenza:string) {
         this.nome = nome
         this.cognome = cognome
         this.eta = eta
         this.id = id
+        this.residenza = residenza
         Persona.stampa() // --> Aggiungere un metodo all' interno del costruttore lo richiama al momento della creazione di un'istanza
         this.stampaNome()
     }
@@ -26,7 +30,7 @@ class Persona {
         console.log(this)
     }
     private stampaNome():void{
-        console.log(this.nome)
+        console.log(this.cognome)
     }
 
     // changeid():void{     
@@ -47,5 +51,35 @@ Persona.stampa() // --> Si puo' richiamare anche fuori dalla classe perche' e' s
 // Utile per passare elementi o metodi alle sottoclassi, ma non alle istanze create
 
 // READONLY e' un elemento che una volta inizializzato non puo' piu' essere cambiato, come una const
+
+
+class Programmatore extends Persona{
+    constructor(nome:string, cognome:string, eta:number, id:number, residenza:string ){
+        super(nome, cognome, eta, id, residenza)
+    }
+}
+const prof = new Programmatore('Stefano', 'Casasola', 35, 213213, 'Gorizia')
+console.log(prof);
+// prof.residenza --> non e' possibile accedervi esternamente perche' e' protected
+// prof.cognome --> non e' possibile accedervi esternamente perche' e' private 
+// Sia residenza che cognome, pero', esistono all'interno dell' istanza come si puo' notare dal console.log
+// I metodi non esistono all' interno di prof perche' essendo static e private non vengono ereditati dall' istanza 
+
+
+// In TS esiste un modo piu' diretto di creare classi che permette di tipizzare ed inizializzare gli elementi in una volta
+class Persona2 {
+    constructor(public nome:string, private cognome:string){
+        console.log(this.cognome)
+    }
+}
+
+const pizzaiolo = new Persona2('Ciro', 'Esposito')
+// Mettendo un qualsiasi modificatore durante la tipizzazione nel constructor e' possibile sia tipizzare gli elementi che assegnargli direttamente
+// il valore inserito come parametro durante la creazione dell' istanza. In questo modo e' possibile risparmiare molte righe di codice in quanto non e' 
+// piu' necessario tipizzare prima gli elementi della classe ed assegnare il valore ad ognuno di essi in seguito
+
+
+
+
 
 
